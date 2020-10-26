bbd6dc72ecc40b55aa4fd317bd9260c59c79c99c
mchaoTechtonic <matthew.chao@techtonic.com>
2020-10-26 17:55:08 -0400

    Fix failing test 1
    -Define final price function
    -Multiplies by percent changes, and rounds to 2 places

--- logic/price.js
+++ logic/price.js
@@ -1,4 +1,15 @@
 function Prices() {
+    function calculateFinalPrice(basePrice,passengerPercentChange,flightPercentChange) {
+        const multiplier1 = (100+passengerPercentChange)/100;
+        const multiplier2 = (100+flightPercentChange)/100;
+        const unrounded = basePrice*multiplier1*multiplier2;
+        const numPlaces = 2;
+        const tenPow = Math.pow(10,numPlaces);
+        return Math.round(tenPow*unrounded)/tenPow;
+    }
 
+    return {calculateFinalPrice};
 }
+
+module.exports = Prices();
 
