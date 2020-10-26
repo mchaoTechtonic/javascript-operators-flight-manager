b6f5698707459ce0028b723a0e2b02641f79e7d7
mchaoTechtonic <matthew.chao@techtonic.com>
2020-10-26 17:06:36 -0400

    Fix failing test #4
    -Defines the checkFlightCapacity function

--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,15 @@
 function Passengers() {
+    function checkFlightCapacity(capacity,numbers) {
+        let totPassengers = numbers.reduce((acc,curr)=>(acc+curr),0);
+        if (totPassengers<=capacity) {
+            return totPassengers;
+        } else {
+            throw new Error("The capacity of the flight is exceeded");
+        }
+    }
 
+    return {checkFlightCapacity};
 }
+
+module.exports = Passengers();
 
