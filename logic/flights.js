dcd9d27869b5c877f1ad51c1f763a32fde13792e
mchaoTechtonic <matthew.chao@techtonic.com>
2020-10-26 16:34:34 -0400

    Fix failing test #2
    -Currently the calculateNumberOfFlights doesn't handle invalid inputs
    -Checks for non-integral or negative values, and throws errors in those cases
    -Also adds a finally block with a console log

--- logic/flights.js
+++ logic/flights.js
@@ -1,5 +1,9 @@
 function Flights() {
     function calculateNumberOfFlights(numberOfPassengers,flightCapacity) {
+        if (!Number.isInteger(numberOfPassengers) || numberOfPassengers<0) {
+            throw new Error('The number of passengers must be a positive integer value');
+        }
+        if (!Number.isInteger(flightCapacity) || flightCapacity<0) throw new Error("The capacity of the flight must be a positive integer value");
         ret = Math.floor(numberOfPassengers/flightCapacity);
         if (numberOfPassengers%flightCapacity) {
             ++ret;
--- logic/frontend.js
+++ logic/frontend.js
@@ -12,7 +12,9 @@ function onCalculateNumberOfFlights() {
           " flight(s) to carry " + passengers + " passengers";
     } catch (error) {
         document.getElementById('flights').innerHTML = error; 
-    } 
+    } finally {
+        console.log("passengers and capacity: ", passengers, capacity);
+    }
 }
 
 function onCalculateTotalFinalPrice(object) {
