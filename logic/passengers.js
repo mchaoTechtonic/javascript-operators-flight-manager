5f1dfa4f7b2ae460211eac57178536333834aef7
mchaoTechtonic <matthew.chao@techtonic.com>
2020-10-26 17:33:58 -0400

    Fix failing test 5
    -Define distribute seats function
    -Do arithmetic for assigning by priority

--- logic/passengers.js
+++ logic/passengers.js
@@ -10,17 +10,41 @@ function Passengers() {
 
     function distributeAllSeatsToAllPassengers(numVIP,numRegular,numFlights,numBizSeatsPerFlight,
         numEconomySeatsPerFlight) {
-        const ret = {};
-        const numBizSeats = numFlights*numBizSeatsPerFlight;
-        const numEconomySeats = numFlights*numEconomySeatsPerFlight;
+        let ret = {};
+        let numBizSeats = numFlights*numBizSeatsPerFlight;
+        let numEconomySeats = numFlights*numEconomySeatsPerFlight;
         let x = 0;
-        x = Math.min(numVIP,numBizSeatsPerFlight);
+        x = Math.min(numVIP,numBizSeats);
         ret.vipPassengersWithBusinessSeats = x;
-        numVip -= x;
+        numVIP -= x;
+        numBizSeats -= x;
 
-        ret.vipPassengersWithEconomySeats = 0;
-        ret.regularPassengersWithBusinessSeats = 0;
-        ret.regularPassengersWithEconomySeats = 0;
+        if (numVIP>0) {
+            x = Math.min(numVIP,numEconomySeats);
+            ret.vipPassengersWithEconomySeats = x;
+            numVIP -= x;
+            numEconomySeats -= x;
+        } else {
+            //all VIPs fit into business class
+            ret.vipPassengersWithEconomySeats = 0;
+        }
+
+        let y = 0;
+        y = Math.min(numRegular,numBizSeats);
+        ret.regularPassengersWithBusinessSeats = y;
+        numRegular -= y;
+        numBizSeats -= y;
+
+        if (numRegular>0) {
+            y = Math.min(numRegular,numEconomySeats);
+            ret.regularPassengersWithEconomySeats = y;
+            numRegular -= y;
+            numEconomySeats -= y;
+        } else {
+            //all regulars fit into business;
+            ret.regularPassengersWithEconomySeats = 0;
+        }
+        return ret;
     }
 
     return {checkFlightCapacity,distributeAllSeatsToAllPassengers};
