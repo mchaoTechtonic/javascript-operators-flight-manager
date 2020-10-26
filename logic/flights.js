e4fdc03c11d36142045c7ca475f1acaf03289a9c
mchaoTechtonic <matthew.chao@techtonic.com>
2020-10-26 16:04:30 -0400

    Fix failing test #1
    -calculateNumberOfFlights was called but not yet defined
    -Define the function, get the arithmetic right, and export the function

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,13 @@
 function Flights() {
+    function calculateNumberOfFlights(numberOfPassengers,flightCapacity) {
+        ret = Math.floor(numberOfPassengers/flightCapacity);
+        if (numberOfPassengers%flightCapacity) {
+            ++ret;
+        }
+        return ret;
+    }
 
+    return {calculateNumberOfFlights};
 }
 
+module.exports = Flights();
